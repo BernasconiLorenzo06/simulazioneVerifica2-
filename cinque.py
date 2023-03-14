8a34253d2c6f549e6539f0a89479fec227c12b2c
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2023-03-14 18:08:03 +0000

    ultimo commit

---
+++ cinque.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+import datetime
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('tour5.html')
+
+@app.route('/persona', methods=['GET'])
+def persona():
+    return render_template('persona.html')
+    
+@app.route('/gruppo', methods=['GET'])
+def gruppo():
+    return render_template('gruppo.html')
+
+@app.route('/guida', methods=['GET'])
+def guida():
+    return render_template('gruppoGuida.html')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
