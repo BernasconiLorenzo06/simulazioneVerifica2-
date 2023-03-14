f5e7cbc8e9fbcb30419e8c3a74b95f23ab13056d
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2023-03-14 17:44:04 +0000

    quarto commit

--- quattro.py
+++ quattro.py
@@ -7,6 +7,17 @@ import datetime
 def home():
     return render_template('tour4.html')
 
+@app.route('/persona', methods=['GET'])
+def persona():
+    return render_template('persona.html')
+    
+@app.route('/gruppo', methods=['GET'])
+def gruppo():
+    return render_template('gruppo.html')
+
+@app.route('/guida', methods=['GET'])
+def guida():
+    return render_template('gruppoGuida.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
